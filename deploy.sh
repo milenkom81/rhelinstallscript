851bac31cf1aa766f4128b2f64ca17d5e41288f4
Milenko Mitrović <milenkom81@gmail.com>
2018-10-03 23:43:31 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,15 @@
 yum install -y java-1.8.0-openjdk-devel
 
 #Set java home
-echo "JAVA_HOME='/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-3.b13.el7_5.x86_64/'" >> /etc/profile
-echo "export JAVA_HOME" >> /etc/profile
-echo 'PATH="$JAVA_HOME/bin:$PATH"' >>/etc/profile
-echo 'export PATH' >>/etc/profile
+echo "JAVA_HOME='/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-3.b13.el7_5.x86_64/'" >> /etc/profile.d/vars.sh
+echo "export JAVA_HOME" >> /etc/profile.d/vars.sh
+echo 'PATH="$JAVA_HOME/bin:$PATH"' >>/etc/profile.d/vars.sh
+echo 'export PATH' >>/etc/profile.d/vars.sh
 
 #Use new env variable
-source /etc/profile
+touch /etc/profile.d/vars.sh
+chmod +x /etc/profile.d/vars.sh
+sh /etc/profile.d/vars.sh
 
 #Install maven
 cd /opt
@@ -20,28 +22,36 @@ wget http://www-eu.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5
 tar -xvzf apache-maven-3.5.4-bin.tar.gz
 
 #Set maven home and paths
-echo 'M2_HOME="/opt/apache-maven-3.5.4"' >>/etc/profile
-echo 'export M2=$M2_HOME/bin' >>/etc/profile
-echo 'export PATH=$M2:$PATH' >>/etc/profile
+echo 'M2_HOME="/opt/apache-maven-3.5.4"' >>/etc/profile.d/vars.sh
+echo 'export M2=$M2_HOME/bin' >>/etc/profile.d/vars.sh
+echo 'export PATH=$M2:$PATH' >>/etc/profile.d/vars.sh
 
 #Use environment
-source /etc/profile
+
+sh /etc/profile.d/vars.sh
 
 #Install tomcat
-yum install tomcat
+yum install -y tomcat
+
+#add parameters for tomcat
+echo 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"'>>/usr/share/tomcat/conf/tomcat.conf
+
+yum install -y tomcat-webapps
+yum install -y tomcat-admin-webapps
+
+
 
 #Make sure that tomcat user has rights to use tomcat folders...
 chown -R tomcat:tomcat /var/lib/tomcat
 
 #Start tomcat service
-service tomcat start
+systemctl start tomcat
 
-
+#Enable tomcat on every boot
+systemctl enable tomcat
 
 
 
 #Enable firewall to allow tomcat
 service firewalld stop
 service firewalld disable
-
-
