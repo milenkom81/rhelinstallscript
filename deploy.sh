f02296c2a2e14b43213e1b9ff4d47a74d3f33421
milenkom81 <Manuela1311>
2018-10-03 22:31:48 +0200

    fix for variabes hope so

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@
 yum install -y java-1.8.0-openjdk-devel
 
 #Set java home
-echo "JAVA_HOME='/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-3.b13.el7_5.x86_64/'" >> /etc/profile
-echo "export JAVA_HOME" >> /etc/profile
-echo 'PATH="$JAVA_HOME/bin:$PATH"' >>/etc/profile
-echo 'export PATH' >>/etc/profile
+echo "JAVA_HOME='/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-3.b13.el7_5.x86_64/'" >> /etc/profile.d/vars.sh
+echo "export JAVA_HOME" >> /etc/profile.d/vars.sh
+echo 'PATH="$JAVA_HOME/bin:$PATH"' >>/etc/profile.d/vars.sh
+echo 'export PATH' >>/etc/profile.d/vars.sh
 
 #Use new env variable
-source /etc/profile
+sh /etc/profile.d/vars.sh
 
 #Install maven
 cd /opt
@@ -20,15 +20,16 @@ wget http://www-eu.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5
 tar -xvzf apache-maven-3.5.4-bin.tar.gz
 
 #Set maven home and paths
-echo 'M2_HOME="/opt/apache-maven-3.5.4"' >>/etc/profile
-echo 'export M2=$M2_HOME/bin' >>/etc/profile
-echo 'export PATH=$M2:$PATH' >>/etc/profile
+echo 'M2_HOME="/opt/apache-maven-3.5.4"' >>/etc/profile.d/vars.sh
+echo 'export M2=$M2_HOME/bin' >>/etc/profile.d/vars.sh
+echo 'export PATH=$M2:$PATH' >>/etc/profile.d/vars.sh
 
 #Use environment
-source /etc/profile
+sh /etc/profile.d/vars.sh
 
 #Install tomcat
-yum install tomcat
+yum install -y tomcat
+
 
 #Make sure that tomcat user has rights to use tomcat folders...
 chown -R tomcat:tomcat /var/lib/tomcat
